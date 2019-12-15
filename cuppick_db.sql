7b21ee51ab41643f56feef25c296dd14afb34e23
btothey99 <55133692+btothey99@users.noreply.github.com>
2019-12-15 18:43:35 +0900

    Update cuppick_db.sql
    최종 DB

--- cuppick_db.sql
+++ cuppick_db.sql
@@ -22,9 +22,9 @@ USE `test` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `test`.`user` (
   `UserID` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `UserType` TINYINT(2) NOT NULL,
   `Email` VARCHAR(100) NOT NULL,
   `Password` VARCHAR(45) NOT NULL,
-  `UserType` TINYINT(2) NOT NULL,
   `Nickname` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`UserID`),
   UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) ,
@@ -34,39 +34,19 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `test`.`common`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test`.`common` (
-  `CommonID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `CafeAddress` VARCHAR(100) NOT NULL,
-  `CafeName` VARCHAR(45) NOT NULL,
-  `user_UserID` INT(11) UNSIGNED NOT NULL,
-  PRIMARY KEY (`CommonID`),
-  UNIQUE INDEX `CommonID_UNIQUE` (`CommonID` ASC) ,
-  INDEX `fk_common_user1_idx` (`user_UserID` ASC) ,
-  CONSTRAINT `fk_common_user1`
-    FOREIGN KEY (`user_UserID`)
-    REFERENCES `test`.`user` (`UserID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
 -- Table `test`.`customer`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `test`.`customer` (
-  `CustomerID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `CustomerID` INT(10) UNSIGNED NOT NULL,
   `Score` INT(10) UNSIGNED NOT NULL,
-  `Post` VARCHAR(1000) NOT NULL,
-  `common_CommonID` INT(10) UNSIGNED NOT NULL,
+  `Post` VARCHAR(2000) NOT NULL,
+  `user_UserID` INT(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`CustomerID`),
   UNIQUE INDEX `CustomerID_UNIQUE` (`CustomerID` ASC) ,
-  INDEX `fk_customer_common1_idx` (`common_CommonID` ASC) ,
-  CONSTRAINT `fk_customer_common1`
-    FOREIGN KEY (`common_CommonID`)
-    REFERENCES `test`.`common` (`CommonID`)
+  INDEX `fk_customer_user1_idx` (`user_UserID` ASC) ,
+  CONSTRAINT `fk_customer_user1`
+    FOREIGN KEY (`user_UserID`)
+    REFERENCES `test`.`user` (`UserID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -77,20 +57,22 @@ DEFAULT CHARACTER SET = utf8;
 -- Table `test`.`owner`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `test`.`owner` (
-  `OwnerID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `OwnerID` INT(10) UNSIGNED NOT NULL,
+  `CafeName` VARCHAR(45) NOT NULL,
+  `CafeAddress` VARCHAR(150) NOT NULL,
   `CafeType` VARCHAR(45) NOT NULL,
   `Purpose` VARCHAR(45) NOT NULL,
   `Plug` TINYINT(2) UNSIGNED NOT NULL,
   `Parking` TINYINT(2) UNSIGNED NOT NULL,
   `Toilet` TINYINT(2) UNSIGNED NOT NULL,
   `WiFi` TINYINT(2) UNSIGNED NOT NULL,
-  `common_CommonID` INT(10) UNSIGNED NOT NULL,
+  `user_UserID` INT(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`OwnerID`),
   UNIQUE INDEX `OwnerID_UNIQUE` (`OwnerID` ASC) ,
-  INDEX `fk_owner_common1_idx` (`common_CommonID` ASC) ,
-  CONSTRAINT `fk_owner_classfication1`
-    FOREIGN KEY (`common_CommonID`)
-    REFERENCES `test`.`common` (`CommonID`)
+  INDEX `fk_owner_user1_idx` (`user_UserID` ASC) ,
+  CONSTRAINT `fk_owner_user1`
+    FOREIGN KEY (`user_UserID`)
+    REFERENCES `test`.`user` (`UserID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
