25b1197af92a969682f7c696abe78d16e11a8b71
hayoung1214 <55429156+hayoung1214@users.noreply.github.com>
2019-12-16 22:06:06 +0900

    Update owner_info_php.php

--- owner_info_php.php
+++ owner_info_php.php
@@ -4,18 +4,15 @@ $host='39.115.75.246';
 $user='bobo';
 $password='opensw1';
 $dbname='test';
-
 $owner_cafename=$_POST['cafename'];
 $owner_address=$_POST['cafeaddress'];
 //변수지정
 $owner_cafetype=$_POST['cafetype'];
 $owner_purpose=$_POST['purpose'];
-
 $owner_plug=$_POST['plug'];
 $owner_parking=$_POST['parking'];
 $owner_wifi=$_POST['wifi'];
 $owner_toilet=$_POST['toilet'];
-
 $db=new mysqli($host, $user, $password, $dbname);
  if(mysqli_connect_errno()){
    echo '<p>Error: Could not connect to database.<br/> Please try agin later.</p>';
@@ -23,20 +20,14 @@ $db=new mysqli($host, $user, $password, $dbname);
  }
 echo $owner_cafetype;
 echo $owner_purpose ;
-
 echo $owner_plug;
 echo $owner_parking;
 echo $owner_toilet;
 echo $owner_wifi ;
-
-
 $userid=$_COOKIE["myvar"];//session=cookie
 // if select count(*) from owner  where userid = $user_userid;
   //   update owner set cafetype = $owner_cafetype , purpose = $
 //$user_query_insert="INSERT INTO owner (ownerid, CafeType, Purpose, Plug, Parking, Toilet, WiFi, common_CommonID) VALUES (?, ?, ?, ?, ? , ?, 1)";
-
-
-
 $user_count="select count(*) from owner where ownerid = ?";
 $stmt3=$db->prepare($user_count);
 $stmt3->bind_param('d',$userid);
@@ -46,15 +37,10 @@ $stmt3->bind_result($count);
 $num=0;
 while($id_row=$stmt3->fetch()){
 }
-
 echo "string   ";
-
 echo $count;
-
 if ($count == 0) {
   echo "insert";
-
-
  $user_query_insert="INSERT INTO owner (ownerid,CafeType, cafename, cafeaddress, Purpose, Plug, Parking, Toilet, WiFi, user_userid)  VALUES (?,?,?, ?, ?, ?, ? , ?, ?,?)";
    $stmt3=$db->prepare($user_query_insert);
    $stmt3->bind_param('dssssddddd',$userid,$owner_cafetype,$owner_cafename,$owner_address,$owner_purpose,$owner_plug,$owner_parking,$owner_toilet,$owner_wifi,$userid);
@@ -72,11 +58,9 @@ $user_query_update="UPDATE owner
  Toilet='$owner_toilet',
   WiFi='$owner_wifi',
    user_userid='$userid'
-
  WHERE ownerid=$userid ";
-
-
  $stmt3=$db->prepare($user_query_update);
  $stmt3->execute();
 }
+Header("Location:./ver2_main.php");
 ?>
