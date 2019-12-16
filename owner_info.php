d16a0cbe1381a9a756e3298f74f5096bb922949c
hayoung1214 <55429156+hayoung1214@users.noreply.github.com>
2019-12-16 22:05:40 +0900

    Update owner_info.php

--- owner_info.php
+++ owner_info.php
@@ -6,43 +6,39 @@
 
 <form action="owner_info_php.php" method="post">
 <p>
-카페 이름
+새로운 카페 이름
  <input type="text" name="cafename" >
 <p>
-카페 주소
+새로운 카페 주소
 <input type="text" name="cafeaddress">
   <p>
-카페 타입
-  <input type="radio" name="cafetype" value="franchise"> 프렌차이즈 <br>
+[카페 타입]<br>
+  <input type="radio" name="cafetype" value="franchise"> 프렌차이즈 
   <input type="radio" name="cafetype" value="independent"> 개인카페 <br>
 <p>
-  카페 목적
+  [카페 목적] <br>
   <input type="radio" name="purpose" value="study">스터디
-  <br>
+ 
   <input type="radio" name="purpose" value="chat"> 수다떨기
   <br><br>
-
   <p>
-  콘센트
-  <input type="radio" name="plug" value="1"> 있음 <br>
+[바뀐 카페기능]<br>
+  <콘센트><br>
+  <input type="radio" name="plug" value="1"> 있음 
   <input type="radio" name="plug" value="0"> 없음 <br>
   <p>
-  주차가능
-  <input type="radio" name="parking" value="1"> 가능 <br>
+  <주차가능><br>
+  <input type="radio" name="parking" value="1"> 가능 
   <input type="radio" name="parking" value="0"> 불가능 <br>
   <p>
-  와이파이
-  <input type="radio" name="wifi" value="1"> 있음 <br>
+  <와이파이><br>
+  <input type="radio" name="wifi" value="1"> 있음 
   <input type="radio" name="wifi" value="0"> 없음 <br>
   <p>
-  카페 내부 화장실
-  <input type="radio" name="toilet" value="1"> 있음 <br>
+  <카페 내부 화장실> <br>
+  <input type="radio" name="toilet" value="1"> 있음
   <input type="radio" name="toilet" value="0"> 없음 <br>
-
  <input type="submit" value="modify">
 </form>
-
-
-
 </body>
 </html>
