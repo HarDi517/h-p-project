56ec6ba8d0336d6f91135c5da53fd2ca2140bb57
yds04312 <55349275+yds04312@users.noreply.github.com>
2019-12-16 17:34:26 +0900

    Create customer_info.php

---
+++ customer_info.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2><strong>view / modify customer information </h2>
+
+<!--쿠키 넣을 자리-->
+<form action="customer_info_php.php" method="post">
+<p>
+닉네임
+ <input type="text" name="nickname" >
+<p>
+비밀번호
+<input type="text" name="password">
+<p>
+
+ <input type="submit" value="modify">
+</form>
+
+
+
+</body>
+</html>
